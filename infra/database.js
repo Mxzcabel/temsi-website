690df9961085cec3596ac130d9e24c972e398190
Mxzcabel <Mxzcabel@users.noreply.github.com>
2024-04-10 17:24:06 -0300

    Update robust database for CA verification and include migrations dynamic

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,8 @@ async function query (queryObject) {
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV === "development" ? false : true,
+        ssl: getSSLValues(),
     });
-    console.log("credenciais do postgres:", {
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            user: process.env.POSTGRES_USER,
-            database: process.env.POSTGRES_DB,
-            password: process.env.POSTGRES_PASSWORD,
-        });
 
     try {
        await client.connect();
@@ -31,5 +24,14 @@ async function query (queryObject) {
 }
 
 export default {
-    query: query
+    query: query,
+}
+
+function getSSLValues() {
+    if (process.env.POSTGRES_CA) {
+        return {
+            ca: process.env.POSTGRES_CA,
+        };
+    }
+    return process.env.NODE_ENV === "development" ? false : true;
 }
